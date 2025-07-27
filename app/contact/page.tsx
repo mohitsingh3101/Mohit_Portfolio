424c974659c4995d19809673667a6f825719b61c
Mohit Singh <mohit31jan2002@gmail.com>
2025-07-27 21:30:59 +0530

    first commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,138 @@
+// import { Button } from "@/components/ui/button"
+// import Link from "next/link"
+// import { BiMapPin , BiPhone } from "react-icons/bi"
+// import { BsArrowLeft } from "react-icons/bs"
+// import { MdMail, MdPhone } from "react-icons/md"
+
+// const page = () => {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center">
+//       <main className="max-w-xl m-3 md:m-8 z-50 w-full space-y-8
+//       border rounded-xl border-gray-700/50 p-5 sm:p-10
+//       backdrop-blur-xl bg-black/80">
+//         <Link href="/">
+//           <Button 
+//           variant="ghost"
+//           className="hover:bg-black hover:text-white rounded-full
+//           mb-5 cursor-pointer">
+//               Back <BsArrowLeft className="ml-2" />
+//           </Button>
+//         </Link>
+
+//         <div className="space-y-6">
+//           <h1 className="text-4xl bg-clip-text font-bold
+//           text-transparent bg-gradient-to-r from-emerald-400
+//           via-sky-300 to-blue-500">Get in Touch</h1>
+
+//           <div className="space-y-6">
+//             <p className="text-pretty text-neutral-200 tracking-tight">
+//                 I'm always open to discussing new projects, creative ideas or
+//                 opportunities to be part of your visions. Feel free to reach out
+//                 through any of the following channels.
+//             </p>
+
+//             <div className="space-y-4">
+//                 <div className="flex items-center gap-3 p-4 rounded-lg
+//                 border border-gray-700/50 bg-black/40">
+//                   <MdMail className="w-5 h-5 text-emerald-400" />
+//                   <div>
+//                       <h3 className="font-medium">Email</h3>
+//                       <a href="mailto:mohit31jan2002@gmail.com"
+//                       className="text-neutral-500 hover:text-emerald-400
+//                       transition-colors"
+//                       >
+//                         mohit31jan2002@gmail.com
+//                       </a>
+//                   </div>
+//                 </div>
+
+//                 <div className="flex items-center gap-3 p-4 rounded-lg
+//                 border border-gray-700/50 bg-black/40">
+//                   <MdPhone className="w-5 h-5 text-emerald-400" />
+//                   <div>
+//                       <h3 className="font-medium">Phone</h3>
+//                       <a href=""
+//                       className="text-neutral-500 hover:text-emerald-400
+//                       transition-colors"
+//                       >
+//                         +91 9518382322
+//                       </a>
+//                   </div>
+//                 </div>
+
+//                 <div className="flex items-center gap-3 p-4 rounded-lg
+//                 border border-gray-700/50 bg-black/40">
+//                   <BiMapPin className="w-5 h-5 text-emerald-400" />
+//                   <div>
+//                       <h3 className="font-medium">Location</h3>
+//                       <p 
+//                       className="text-neutral-500 hover:text-emerald-400
+//                       transition-colors cursor-pointer"
+//                       >
+//                         Nagpur, India
+//                       </p>
+//                   </div>
+//                 </div>
+//             </div>
+
+//             <div className="space-y-4">
+//                 <h2 className="text-2xl font-semibold">Send Me a Message</h2>
+
+//                 <form action="https://formsubmit.co/mohit31jan2002@gmail.com" method="POST" className="space-y-4">
+//                   <div className="space-y-2">
+//                     <label htmlFor="" className="text-sm font-medium">
+//                       Name
+//                     </label>
+//                     <input type="text" id="name" placeholder="Your name"
+//                     className="w-full p-2 rounded-lg flex items-center gap-3 border
+//                     border-gray-700/50 bg-black/40 focus:outline-none focus:ring-2
+//                     focus:ring-emrald-500 transition-all duration-300" />
+//                   </div>
+
+//                   <div className="space-y-2">
+//                     <label htmlFor="" className="text-sm font-medium">
+//                       Email
+//                     </label>
+//                     <input type="text" id="name" placeholder="Your Email"
+//                     className="w-full p-2 rounded-lg flex items-center gap-3 border
+//                     border-gray-700/50 bg-black/40 focus:outline-none focus:ring-2
+//                     focus:ring-emrald-500 transition-all duration-300" />
+//                   </div>
+
+//                   <div className="space-y-2">
+//                     <label htmlFor="" className="text-sm font-medium">
+//                       Message
+//                     </label>
+//                     <textarea name="" id="message" rows={5}
+//                     placeholder="Your Mesage"
+//                     className="w-full p-2 rounded-lg flex items-center
+//                     gap-3 border border-gray-700/50 bg-black/40
+//                     focus:outline-none focus:ring-2 focus-emerald-500
+//                     transition-all duration-300"
+//                     ></textarea>
+//                   </div>
+
+//                   <Button type="submit" className="w-full text-black bg-emerald-400
+//                   hover:bg-emerald-500">Send Message</Button>
+
+//                 </form>
+
+//             </div>
+
+//         </div>
+
+//         </div>
+
+//       </main>
+//     </div>
+//   )
+// }
+
+// export default page
+  
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { BiMapPin , BiPhone } from "react-icons/bi"
+import { BiMapPin } from "react-icons/bi"
 import { BsArrowLeft } from "react-icons/bs"
 import { MdMail, MdPhone } from "react-icons/md"
 
@@ -26,7 +158,7 @@ const page = () => {
 
           <div className="space-y-6">
             <p className="text-pretty text-neutral-200 tracking-tight">
-                I'm always open to discussing new projects, creative ideas or
+                I&apos;m always open to discussing new projects, creative ideas or
                 opportunities to be part of your visions. Feel free to reach out
                 through any of the following channels.
             </p>
@@ -86,17 +218,17 @@ const page = () => {
                     <input type="text" id="name" placeholder="Your name"
                     className="w-full p-2 rounded-lg flex items-center gap-3 border
                     border-gray-700/50 bg-black/40 focus:outline-none focus:ring-2
-                    focus:ring-emrald-500 transition-all duration-300" />
+                    focus:ring-emerald-500 transition-all duration-300" />
                   </div>
 
                   <div className="space-y-2">
                     <label htmlFor="" className="text-sm font-medium">
                       Email
                     </label>
-                    <input type="text" id="name" placeholder="Your Email"
+                    <input type="text" id="email" placeholder="Your Email"
                     className="w-full p-2 rounded-lg flex items-center gap-3 border
                     border-gray-700/50 bg-black/40 focus:outline-none focus:ring-2
-                    focus:ring-emrald-500 transition-all duration-300" />
+                    focus:ring-emerald-500 transition-all duration-300" />
                   </div>
 
                   <div className="space-y-2">
@@ -104,10 +236,10 @@ const page = () => {
                       Message
                     </label>
                     <textarea name="" id="message" rows={5}
-                    placeholder="Your Mesage"
+                    placeholder="Your Message"
                     className="w-full p-2 rounded-lg flex items-center
                     gap-3 border border-gray-700/50 bg-black/40
-                    focus:outline-none focus:ring-2 focus-emerald-500
+                    focus:outline-none focus:ring-2 focus:ring-emerald-500
                     transition-all duration-300"
                     ></textarea>
                   </div>
@@ -129,4 +261,3 @@ const page = () => {
 }
 
 export default page
-  
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,170 @@
+// import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import{
+//   BiBuilding,
+//   BiChevronRight,
+//   BiLogoGithub,
+//   BiLogoInstagram,
+//   BiLogoTiktok,
+//   BiLogoYoutube,
+//   BiLogoLinkedin,
+// } from "react-icons/bi";
+// import { HiArrowDown } from "react-icons/hi";
+
+
+// export default function Home() {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center">
+//       <main className="w-full max-w-xl m-3 md:m-8 z-50 space-y-8 border border-gray-700/50
+//       rounded-xl p-5 sm:p-10 backdrop-blur-xl bg-black/80">
+
+//         <div className="flex sm:flex-row sm:justify-between sm:items-center flex-col-reverse 
+//         items-start gap-3 sm:gap-10">
+//             <div className="flex items-center gap-3">
+//               <BiBuilding className="w-5 h-5"/>
+//               <h3 className="text-gray-400"> Nagpur, India</h3>
+//             </div>
+
+//             <button className="flex hover:bg-black/50 duration-300
+//             hover:text-white rounded-full">
+//               <div className="relative flex text-sm gap-1 items-center justify-center h-3
+//               w-3 rounded-full">
+//                 <span className="animate-ping absolute inline-flex h-full w-full
+//                 rounded-full bg-green-500"></span>
+//                 <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+//               </div>
+//               <p>Available For Work</p>
+//             </button>
+//         </div>
+
+//         <div>
+//           <h1 className="text-4xl">Mohit Singh</h1>
+//           <h3 className="text-xl font-bold mt-2 bg-clip-text text-transparent bg-gradient-to-r
+//           from-emerald-400 via-sky-300 to-blue-500">
+//             SDE Aspirant
+//           </h3>
+//           <p className="text-pretty tracking-tight mt-3 text-neutral-300">
+//               Hello, I’m a Web Developer and SDE aspirant with 400+ DSA problems
+//                solved and hands-on experience in frontend development.
+//               {" "}
+//               <Link href="/contact"
+//               className="bg-clip-text text-transparent bg-gradient-to-r
+//           from-emerald-400 via-sky-300 to-blue-500 font-bold opacity-90
+//            hover:opacity-100 transition-opacity">
+//                 Contact me{" "}
+//                </Link>{" "}
+//                for collaboration or freelance work anytime.
+//           </p>
+//         </div>
+
+//         <nav>
+//           <h3 className="text-xl font-bold text-emerald-400">Menu</h3>
+
+//           <div className="flex flex-col gap-3">
+//             <Link href="/about" className="w-1/2 border bg-black/30 border-gray-700/40
+//             flex group items-center justify-between p-3 cursor-pointer transition-colors
+//             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
+//                 <span>About</span>
+//                 <BiChevronRight className="mr-5 text-2xl translate-x-0
+//                 group-hover:translate-x-3 text-gray-300 opacity-0
+//                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
+//             </Link>
+//             <Link href="/work" className="w-1/2 border bg-black/30 border-gray-700/40
+//             flex group items-center justify-between p-3 cursor-pointer transition-colors
+//             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
+//                 <span>Work</span>
+//                 <BiChevronRight className="mr-5 text-2xl translate-x-0
+//                 group-hover:translate-x-3 text-gray-300 opacity-0
+//                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
+//             </Link>
+//             <Link href="/skills" className="w-1/2 border bg-black/30 border-gray-700/40
+//             flex group items-center justify-between p-3 cursor-pointer transition-colors
+//             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
+//                 <span>Skills</span>
+//                 <BiChevronRight className="mr-5 text-2xl translate-x-0
+//                 group-hover:translate-x-3 text-gray-300 opacity-0
+//                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
+//             </Link>
+//             <Link href="/contact" className="w-1/2 border bg-black/30 border-gray-700/40
+//             flex group items-center justify-between p-3 cursor-pointer transition-colors
+//             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
+//                 <span>Contact</span>
+//                 <BiChevronRight className="mr-5 text-2xl translate-x-0
+//                 group-hover:translate-x-3 text-gray-300 opacity-0
+//                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
+//             </Link>
+//           </div>
+
+//           <div className="flex gap-2 items-center mt-5 mb-5">
+//               <h3 className="text-xl bg-clip-text font-bold text-transparent 
+//               bg-gradient-to-r from-emerald-400 via-sky-400 to-blue-400">
+//                   Check out my socials
+//               </h3>
+//               <HiArrowDown className="text-2xl text-sky-400">
+//               </HiArrowDown>
+//           </div>
+
+//           <div className="flex flex-col gap-3">
+//               <button className="bg-black/30 w-full p-3 text-gray-300
+//               border border-gray-700/40 hover:bg-slate-300/20
+//               hover:text-white transition-colors duration-200
+//               ease-linear flex justify-center items-center gap-2">
+//                 <BiLogoGithub/> GitHub
+//               </button>
+
+//               {/* <button className="bg-black/30 w-full p-3 text-gray-300
+//               border border-gray-700/40 hover:bg-slate-300/20
+//               hover:text-white transition-colors duration-200
+//               ease-linear flex justify-center items-center gap-2">
+//                 <BiLogoLinkedin/> LinkdIn
+//               </button> */}
+
+//               <a href="https://www.linkedin.com/in/mohitsingh3101/" target="_blank">
+//                 <button className="bg-black/30 w-full p-3 text-gray-300
+//                 border border-gray-700/40 hover:bg-slate-300/20
+//                 hover:text-white transition-colors duration-200
+//                 ease-linear flex justify-center items-center gap-2">
+//                   <BiLogoLinkedin/> LinkdIn
+//                 </button>
+//               </a>
+
+//               {/* <button  className="bg-black/30 w-full p-3 text-gray-300
+//               border border-gray-700/40 hover:bg-slate-300/20
+//               hover:text-white transition-colors duration-200
+//               ease-linear flex justify-center items-center gap-2">
+//                 <BiLogoInstagram/> Instagram
+//               </button> */}
+
+
+//               <a href="https://www.instagram.com/mohiiiiiiiitt/" target="_blank">
+//                   <button  className="bg-black/30 w-full p-3 text-gray-300
+//                   border border-gray-700/40 hover:bg-slate-300/20
+//                   hover:text-white transition-colors duration-200
+//                   ease-linear flex justify-center items-center gap-2">
+//                     <BiLogoInstagram/> Instagram
+//                   </button>
+//               </a>
+
+              
+
+//           </div>
+
+//         </nav>
+
+//       </main>
+//     </div>
+//   );
+// }
+
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import{
+import {
   BiBuilding,
   BiChevronRight,
   BiLogoGithub,
   BiLogoInstagram,
-  BiLogoTiktok,
-  BiLogoYoutube,
   BiLogoLinkedin,
 } from "react-icons/bi";
 import { HiArrowDown } from "react-icons/hi";
-
 
 export default function Home() {
   return (
@@ -20,21 +174,21 @@ export default function Home() {
 
         <div className="flex sm:flex-row sm:justify-between sm:items-center flex-col-reverse 
         items-start gap-3 sm:gap-10">
-            <div className="flex items-center gap-3">
-              <BiBuilding className="w-5 h-5"/>
-              <h3 className="text-gray-400"> Nagpur, India</h3>
-            </div>
+          <div className="flex items-center gap-3">
+            <BiBuilding className="w-5 h-5" />
+            <h3 className="text-gray-400"> Nagpur, India</h3>
+          </div>
 
-            <button className="flex hover:bg-black/50 duration-300
+          <button className="flex hover:bg-black/50 duration-300
             hover:text-white rounded-full">
-              <div className="relative flex text-sm gap-1 items-center justify-center h-3
+            <div className="relative flex text-sm gap-1 items-center justify-center h-3
               w-3 rounded-full">
-                <span className="animate-ping absolute inline-flex h-full w-full
+              <span className="animate-ping absolute inline-flex h-full w-full
                 rounded-full bg-green-500"></span>
-                <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-              </div>
-              <p>Available For Work</p>
-            </button>
+              <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            </div>
+            <p>Available For Work</p>
+          </button>
         </div>
 
         <div>
@@ -44,16 +198,16 @@ export default function Home() {
             SDE Aspirant
           </h3>
           <p className="text-pretty tracking-tight mt-3 text-neutral-300">
-              Hello, I’m a Web Developer and SDE aspirant with 400+ DSA problems
-               solved and hands-on experience in frontend development.
-              {" "}
-              <Link href="/contact"
+            Hello, I’m a Web Developer and SDE aspirant with 400+ DSA problems
+            solved and hands-on experience in frontend development.
+            {" "}
+            <Link href="/contact"
               className="bg-clip-text text-transparent bg-gradient-to-r
           from-emerald-400 via-sky-300 to-blue-500 font-bold opacity-90
            hover:opacity-100 transition-opacity">
-                Contact me{" "}
-               </Link>{" "}
-               for collaboration or freelance work anytime.
+              Contact me{" "}
+            </Link>{" "}
+            for collaboration or freelance work anytime.
           </p>
         </div>
 
@@ -64,93 +218,72 @@ export default function Home() {
             <Link href="/about" className="w-1/2 border bg-black/30 border-gray-700/40
             flex group items-center justify-between p-3 cursor-pointer transition-colors
             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
-                <span>About</span>
-                <BiChevronRight className="mr-5 text-2xl translate-x-0
+              <span>About</span>
+              <BiChevronRight className="mr-5 text-2xl translate-x-0
                 group-hover:translate-x-3 text-gray-300 opacity-0
                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
             </Link>
             <Link href="/work" className="w-1/2 border bg-black/30 border-gray-700/40
             flex group items-center justify-between p-3 cursor-pointer transition-colors
             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
-                <span>Work</span>
-                <BiChevronRight className="mr-5 text-2xl translate-x-0
+              <span>Work</span>
+              <BiChevronRight className="mr-5 text-2xl translate-x-0
                 group-hover:translate-x-3 text-gray-300 opacity-0
                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
             </Link>
             <Link href="/skills" className="w-1/2 border bg-black/30 border-gray-700/40
             flex group items-center justify-between p-3 cursor-pointer transition-colors
             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
-                <span>Skills</span>
-                <BiChevronRight className="mr-5 text-2xl translate-x-0
+              <span>Skills</span>
+              <BiChevronRight className="mr-5 text-2xl translate-x-0
                 group-hover:translate-x-3 text-gray-300 opacity-0
                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
             </Link>
             <Link href="/contact" className="w-1/2 border bg-black/30 border-gray-700/40
             flex group items-center justify-between p-3 cursor-pointer transition-colors
             duration-200 ease-linear hover:bg-zinc-800/40 rounded-lg">
-                <span>Contact</span>
-                <BiChevronRight className="mr-5 text-2xl translate-x-0
+              <span>Contact</span>
+              <BiChevronRight className="mr-5 text-2xl translate-x-0
                 group-hover:translate-x-3 text-gray-300 opacity-0
                 group-hover:opacity-100 transition-all duration-200"></BiChevronRight>
             </Link>
           </div>
 
           <div className="flex gap-2 items-center mt-5 mb-5">
-              <h3 className="text-xl bg-clip-text font-bold text-transparent 
+            <h3 className="text-xl bg-clip-text font-bold text-transparent 
               bg-gradient-to-r from-emerald-400 via-sky-400 to-blue-400">
-                  Check out my socials
-              </h3>
-              <HiArrowDown className="text-2xl text-sky-400">
-              </HiArrowDown>
+              Check out my socials
+            </h3>
+            <HiArrowDown className="text-2xl text-sky-400" />
           </div>
 
           <div className="flex flex-col gap-3">
+            <button className="bg-black/30 w-full p-3 text-gray-300
+              border border-gray-700/40 hover:bg-slate-300/20
+              hover:text-white transition-colors duration-200
+              ease-linear flex justify-center items-center gap-2">
+              <BiLogoGithub /> GitHub
+            </button>
+
+            <a href="https://www.linkedin.com/in/mohitsingh3101/" target="_blank">
               <button className="bg-black/30 w-full p-3 text-gray-300
-              border border-gray-700/40 hover:bg-slate-300/20
-              hover:text-white transition-colors duration-200
-              ease-linear flex justify-center items-center gap-2">
-                <BiLogoGithub/> GitHub
-              </button>
-
-              {/* <button className="bg-black/30 w-full p-3 text-gray-300
-              border border-gray-700/40 hover:bg-slate-300/20
-              hover:text-white transition-colors duration-200
-              ease-linear flex justify-center items-center gap-2">
-                <BiLogoLinkedin/> LinkdIn
-              </button> */}
-
-              <a href="https://www.linkedin.com/in/mohitsingh3101/" target="_blank">
-                <button className="bg-black/30 w-full p-3 text-gray-300
                 border border-gray-700/40 hover:bg-slate-300/20
                 hover:text-white transition-colors duration-200
                 ease-linear flex justify-center items-center gap-2">
-                  <BiLogoLinkedin/> LinkdIn
-                </button>
-              </a>
+                <BiLogoLinkedin /> LinkedIn
+              </button>
+            </a>
 
-              {/* <button  className="bg-black/30 w-full p-3 text-gray-300
-              border border-gray-700/40 hover:bg-slate-300/20
-              hover:text-white transition-colors duration-200
-              ease-linear flex justify-center items-center gap-2">
-                <BiLogoInstagram/> Instagram
-              </button> */}
-
-
-              <a href="https://www.instagram.com/mohiiiiiiiitt/" target="_blank">
-                  <button  className="bg-black/30 w-full p-3 text-gray-300
+            <a href="https://www.instagram.com/mohiiiiiiiitt/" target="_blank">
+              <button className="bg-black/30 w-full p-3 text-gray-300
                   border border-gray-700/40 hover:bg-slate-300/20
                   hover:text-white transition-colors duration-200
                   ease-linear flex justify-center items-center gap-2">
-                    <BiLogoInstagram/> Instagram
-                  </button>
-              </a>
-
-              
-
+                <BiLogoInstagram /> Instagram
+              </button>
+            </a>
           </div>
-
         </nav>
-
       </main>
     </div>
   );
